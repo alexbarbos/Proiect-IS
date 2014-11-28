d55f6d23441ac26b9776875414fb99b0b3b1397e
cristianStirbe <cristi_carlos_tc@yahoo.com>
2014-11-28 17:34:33 +0200

    Create User.java

---
+++ User.java
@@ -0,0 +1,32 @@
+package model;
+
+public class User {
+
+    private int userid;
+    private String userName;
+    private String password;
+    
+    public int getUserid() {
+        return userid;
+    }
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "User [userid=" + userid + ", userName=" + userName
+                + ", password=" + password + "]";
+    }    
+}
