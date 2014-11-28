ab1d9f2e0a60f2f3dd4c122f168ae727d9453e81
cristianStirbe <cristi_carlos_tc@yahoo.com>
2014-11-28 17:31:38 +0200

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,71 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.UserDao;
+import model.User;
+
+public class UserController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static String INSERT_OR_EDIT = "/user.jsp";
+    private static String LIST_USER = "/listUser.jsp";
+    private static String LOGIN_USER = "/login.jsp";
+    private UserDao dao;
+
+    public UserController() {
+        super();
+        dao = new UserDao();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String forward="";
+        String action = request.getParameter("action");
+
+        if (action.equalsIgnoreCase("delete")){
+            int userId = Integer.parseInt(request.getParameter("userId"));
+            dao.deleteUser(userId);
+            forward = LIST_USER;
+            request.setAttribute("users", dao.getAllUsers());    
+        } else if (action.equalsIgnoreCase("edit")){
+            forward = INSERT_OR_EDIT;
+            int userId = Integer.parseInt(request.getParameter("userId"));
+            User user = dao.getUserById(userId);
+            request.setAttribute("user", user);
+        } else if (action.equalsIgnoreCase("listUser")){
+            forward = LIST_USER;
+            request.setAttribute("users", dao.getAllUsers());
+        } else if (action.equalsIgnoreCase("login")) {
+        	forward = LOGIN_USER;
+        } else {
+            forward = INSERT_OR_EDIT;
+        }
+
+        RequestDispatcher view = request.getRequestDispatcher(forward);
+        view.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        user.setUserName(request.getParameter("userName"));
+        user.setPassword(request.getParameter("password"));
+        
+        String userid = request.getParameter("userid");
+        if(userid == null || userid.isEmpty())
+        {
+            dao.addUser(user);
+        }
+        else
+        {
+            user.setUserid(Integer.parseInt(userid));
+            dao.updateUser(user);
+        }
+        
+        RequestDispatcher view = request.getRequestDispatcher(LIST_USER);
+        request.setAttribute("users", dao.getAllUsers());
+        view.forward(request, response);
+    }
+}
